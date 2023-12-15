754fa2792290eacc4c73f4d159fb96b7e3606035
den <emailfordenis@yandex.ru>
2023-12-16 08:27:34 +1000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char first[30] = "string 1";
+    char second[30] = "string 2: I’m using strncpy now";
+    /* this function has copied first 10 chars of s2 into s1*/
+    strncpy(first, second, 20);
+    printf("String s1 is: %s", first);
+    return 0;
+}
